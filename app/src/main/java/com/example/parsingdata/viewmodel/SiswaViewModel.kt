ce6da74cc0bc2dee4a79302f14ab3e67338c3101
asyiraafnufail <asyiraaf4@gmail.com>
2025-11-20 12:51:10 +0700

    Menambahkan class

--- app/src/main/java/com/example/parsingdata/viewmodel/SiswaViewModel.kt
+++ app/src/main/java/com/example/parsingdata/viewmodel/SiswaViewModel.kt
@@ -1,4 +1,14 @@
 package com.example.parsingdata.viewmodel
 
-class SiswaViewModel {
+import androidx.lifecycle.ViewModel
+import com.example.parsingdata.model.Siswa
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+
+class SiswaViewModel : ViewModel (){
+    private val _statusUI = MutableStateFlow(siswa())
+            val statusUI: StateFlow<Siswa> = _statusUI.asStateFlow()
+
+
 }
